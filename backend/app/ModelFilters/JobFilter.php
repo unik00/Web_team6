f4c464617fb0192a3cd2e84563824387adfbc15c
Hungs20 <hungs20.uet@gmail.com>
2020-08-15 03:19:23 +0700

    search job

--- backend/app/ModelFilters/JobFilter.php
+++ backend/app/ModelFilters/JobFilter.php
@@ -12,7 +12,9 @@ class JobFilter extends ModelFilter
     *
     * @var array
     */
-    public $relations = [];
+    public $relations = [
+       // 'program__language__jobs' => ['job', 'program_language'],
+    ];
     public function name($name)
     {
         return $this->where(function($q) use ($name)
@@ -20,4 +22,38 @@ class JobFilter extends ModelFilter
             return $q->where('name', 'LIKE', "%$name%");
         });
     }
+    public function type($type_id)
+    {
+        return $this->where('type_id', $type_id);
+    }
+    public function payMin($pay_rate)
+    {
+        return $this->where('pay_rate', '>=', $pay_rate);
+    }
+    public function payMax($pay_rate)
+    {
+        return $this->where('pay_rate', '<=', $pay_rate);
+    }
+    public function user($user_id){
+        return $this->where('user_id', $user_id);
+    }
+    public function experience($id){
+        return $this->where('experience_id', $id);
+    }
+    public function country($id){
+        return $this->where('country_id', $id);
+    }
+    public function availabilty($id){
+        return $this->where('availabilty_id', $id);
+    }
+   /*public function programlanguagejobSetup($query)
+    { 
+        echo "a";
+        return $query->programLanguage();
+    }*/
+    public function programLanguage($id){
+        
+        return $this->related('programlanguagejob', 'program_language_id' ,'=', $id);
+    }
+
 }
